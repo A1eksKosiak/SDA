8947a56ef12799cfe5905a62c1828b56dea64b9d
Aleks Kosiak <a1ekskosyak@gmail.com>
2018-08-14 19:16:40 +0300

    Initial commit

--- src/week8/day2/StringToSet.java
+++ src/week8/day2/StringToSet.java
@@ -1,5 +1,6 @@
 package week8.day2;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -8,11 +9,8 @@ public class StringToSet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
-        Set<Character> set = new HashSet<>();
-        char[] array = input.toCharArray();
-        for (int i = 0; i < array.length; i++) {
-            set.add(array[i]);
-        }
+        Character[] charObjectArray = input.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        Set<Character> set = new HashSet<>(Arrays.asList(charObjectArray));
         System.out.println(set.size());
     }
 }
