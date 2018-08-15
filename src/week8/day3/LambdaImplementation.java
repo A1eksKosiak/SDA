6922a5e2c7ab8b3e07630b9446846ba0287bcad0
Aleks Kosiak <a1ekskosyak@gmail.com>
2018-08-15 20:56:40 +0300

    Initial commit

--- src/week8/day3/LambdaImplementation.java
+++ src/week8/day3/LambdaImplementation.java
@@ -1,31 +1,60 @@
 package week8.day3;
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class LambdaImplementation {
     public static void main(String[] args) {
 
-        runItSafe(LambdaImplementation::divide4By, 2);
+        runItSafeConsumer(LambdaImplementation::divide4By, 2);
 
-        runItSafeTwoArguments(LambdaImplementation::divide, 2, 10);
+        runItSafeBiConsumer(LambdaImplementation::divide, 2, 10);
         //Lambda implementation
-        runItSafeTwoArguments((arg1, arg2) -> {
+        runItSafeBiConsumer((arg1, arg2) -> {
             int ans = arg1 / arg2;
             System.out.println(ans);
         }, 2, 10);
-        runItSafeTwoArguments((arg1, arg2) -> System.out.println(arg1 / arg2), 10, 5);  //Lambda implementation
+        runItSafeBiConsumer((arg1, arg2) -> System.out.println(arg1 / arg2), 10, 5);  //Lambda implementation
 
-//        int res = runItSafeReturnInteger(() -> {
+//        int res = runItSafeSupplier(() -> {
 //            return 20 / 2;
 //        });
 //        System.out.println(res);
-        System.out.println(runItSafeReturnInteger(() -> 20 / 2));
+        System.out.println(runItSafeSupplier(() -> 20 / 2));
+
+        System.out.println(runItSafeSupplier(() -> {
+            int sum = 0;
+            for (int i = 1; i <= 50; i++) {
+                sum += i;
+            }
+            return sum;
+        }));
+
+        //Function takes value and returns value
+        System.out.println(runItSafeFunction((element) -> 10 / element, 5));
+        //BiFunction takes 2 parameters and returns one value
+        System.out.println(runItSafeBiFunction((element1, element2) -> element1 / element2, 50, 2));
 
     }
 
-    public static Integer runItSafeReturnInteger(Supplier<Integer> func) {
+    public static Integer runItSafeFunction(Function<Integer, Integer> func, Integer arg1) {
+        try {
+            return func.apply(arg1);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+
+    public static Integer runItSafeBiFunction(BiFunction<Integer, Integer, Integer> func, Integer arg1, Integer arg2) {
+        try {
+            return func.apply(arg1, arg2);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+
+    public static Integer runItSafeSupplier(Supplier<Integer> func) {
         try {
             return func.get();
         } catch (RuntimeException e) {
@@ -34,7 +63,7 @@ public class LambdaImplementation {
         }
     }
 
-    public static void runItSafe(Consumer<Integer> func, Integer arg) {
+    public static void runItSafeConsumer(Consumer<Integer> func, Integer arg) {
         try {
             func.accept(arg);
         } catch (Exception e) {
@@ -42,7 +71,7 @@ public class LambdaImplementation {
         }
     }
 
-    public static void runItSafeTwoArguments(BiConsumer<Integer, Integer> func, Integer arg1, Integer arg2) {
+    public static void runItSafeBiConsumer(BiConsumer<Integer, Integer> func, Integer arg1, Integer arg2) {
         try {
             func.accept(arg1, arg2);
         } catch (Exception e) {
