3d53c248d32a248e18f2a59939479acc908660fd
Aleks Kosiak <a1ekskosyak@gmail.com>
2018-08-16 19:29:21 +0300

    Initial commit Threading

--- src/week8/day4/Threading.java
+++ src/week8/day4/Threading.java
@@ -4,33 +4,46 @@ public class Threading {
 
     public static void main(String[] args) {
 
-        TestThread testThread = new TestThread();
-        //None-deamon threads will not wait deamon thread for finishing the process (program)
-        testThread.setDaemon(true);
-        testThread.start();
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Main thread: " + i);
-            try {
-                Thread.sleep(600);
-            } catch (InterruptedException e) {
-                e.getMessage();
-            }
-        }
+        Chair chair = new Chair();
+        TestThread guy1 = new TestThread(chair);
+        TestThread guy2 = new TestThread(chair);
+        //None-daemon threads will not wait daemon thread for finishing the process (program)
+        guy1.start();
+        guy2.start();
+    }
+}
 
+class Chair {
+    private int availableChair = 1;
+
+    //synchronized opens access to only 1 thread at a time
+    synchronized void takeChair() {
+        if (availableChair > 0) {
+            System.out.println("Taking this chair by " + Thread.currentThread().getName());
+            availableChair--;
+            System.out.println("Current available chairs - " + availableChair);
+        } else {
+            System.out.println("Chair is not available :(");
+        }
     }
 }
 
 class TestThread extends Thread {
 
+    private Chair chair;
+
+    public TestThread(Chair chair) {
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.getMessage();
+//        }
+        this.chair = chair;
+
+    }
+
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Test thread: " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.getMessage();
-            }
-        }
+        chair.takeChair();
     }
 }
