fa16ad2b3b8d78860b5ab2d10f1937e0eea0af77
Aleks Kosiak <a1ekskosyak@gmail.com>
2018-08-17 19:18:52 +0300

    Initial commit Reading from File

--- src/week8/day5/InputOutputToFile.java
+++ src/week8/day5/InputOutputToFile.java
@@ -5,15 +5,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class InputOutputToFile {
     public static void main(String[] args) throws IOException {
         Path filePath = Paths.get("C:\\Users\\A1eks\\IdeaProjects\\SDA\\src\\week8\\day5\\test.txt");
         List<String> lines = Files.readAllLines(filePath);
+        List<Integer> arrayList = new ArrayList<>();
         for (String line : lines) {
-            Integer currentNumber = Integer.parseInt(line);
-            System.out.println(line);
+            arrayList.add(Integer.parseInt(line));
         }
+        System.out.println(arrayList);
+        Integer max = arrayList.get(0);
+        Integer sum = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) > max) {
+                max = arrayList.get(i);
+            }
+            sum += arrayList.get(i);
+        }
+        System.out.println("Max = " + max);
+        System.out.println("Sum = " + sum);
+        Collections.sort(arrayList);
+        System.out.println("Array after sort: " + arrayList);
     }
 }
